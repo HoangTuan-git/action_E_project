74420b0ba05bd546b6bbb12913d736cf3cbd1ae3
HoangTuan-git <hoangtuanmai04@gmail.com>
2025-10-30 15:42:29 +0700

    Done

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -84,7 +84,7 @@ class ProductController {
     try {
       const {id} = req.params;
       const products = await this.productService.getProductById(id);
-      if(!product){
+      if(!products){
         return res.status(404).json({message:"Product not found"});
       }
       return res.status(200).json(products);
